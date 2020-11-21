94ac9ac24bf7c2c30524780bbad83a019603ef11
kumaranimesh97 <53708833+kumaranimesh97@users.noreply.github.com>
2020-11-21 19:42:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var randomNumber1 = 1 + Math.floor( Math.random() * 6 );
+var randomNumber2 = 1 + Math.floor( Math.random() * 6 );
+
+var randomDice1 = "dice" + randomNumber1 + ".png";
+var randomDice2 = "dice" + randomNumber2 + ".png";
+
+var randomImageDice1 = "images/" + randomDice1 ;
+var randomImageDice2 = "images/" + randomDice2 ;
+
+var image1 = document.querySelectorAll("img")[0];
+var image2 = document.querySelectorAll("img")[1];
+
+image1.setAttribute("src" , randomImageDice1);
+image2.setAttribute("src" , randomImageDice2);
+
+if (randomNumber1 > randomNumber2)
+{
+  document.querySelector("h1").innerHTML = " player 1 wins! ";
+}
+else if (randomNumber1 < randomNumber2)
+{
+  document.querySelector("h1").innerHTML = " player 2 wins! ";
+}
+else
+{
+   document.querySelector("h1").innerHTML = " Draw! ";
+}
